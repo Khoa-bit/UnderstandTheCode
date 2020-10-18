e8dc8c866df6875ad31334290a2b6ce5b0c6513e
Khoa <nguyenanhkhoablue@gmail.com>
2020-10-18 18:10:33 +0700

    Make a window with Jframe

---
+++ src/com/barebone/gui/GUI.java
@@ -0,0 +1,30 @@
+package com.barebone.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GUI extends JFrame {
+    public static final int WIDTHJF = 905;
+    public static final int HEIGHTJF = 610;
+
+    public GUI() {
+        setSize(WIDTHJF, HEIGHTJF);
+        setLayout(new CardLayout());
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+//        MyContainer mContainer = new MyContainer(this);
+//        add(mContainer);
+//        WindowAdapter mwindow = new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent e) {
+//                GameSound.getIstance().stop();
+//                PlayGame.IS_RUNNING = false;
+//            }
+//        };
+//        addWindowListener(mwindow);
+    }
+
+}
---
+++ src/com/barebone/startGame/Main.java
@@ -0,0 +1,9 @@
+package com.barebone.startGame;
+import com.barebone.gui.GUI;
+
+
+public class Main {
+    public static void main(String[] args) {
+        new GUI().setVisible(true);
+    }
+}
